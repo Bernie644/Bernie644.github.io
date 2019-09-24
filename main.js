08a735f0d8285b61a1ace1cc70b7ec243667539d
unknown <gmallowpuff@gmail.com>
2019-09-24 16:57:26 +1200

    Assigning array values

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const ajaxPhpBtn = document.getElementById('ajax-php-btn')
 
 const xhr = new XMLHttpRequest()
 
-const loadtxt = () => {                                                         
+const loadtxt = () => {                                                        
   xhr.open('GET', 'nebula.txt', true)                              //Starts the process of getting the document 
   xhr.onload = () => {                                          
     if (xhr.status === 200) {                                      //200 means ok 
@@ -14,6 +14,53 @@ const loadtxt = () => {
   }
   xhr.send()
 }
+const loadjson = () => {    //varible that wont change 
+  xhr.open('GET', 'identity.json', true)
+  xhr.onload = () => {                                          
+    if (xhr.status === 200) {                                      //200 means ok 
+      console.log(xhr)
+      console.log(xhr.responseText)
+      let example = "<ul>";
+      let writeText = JSON.parse (xhr.responseText)   //takes a string and turn it into an object       stringafy takes an object makes it a string
+      writeText.forEach(item => {
+        console.log(item);
+        console.log(item ["firstName"]);
+        console.log(item["lastName"]);
+        console.log(item["emailAddress"]);
+        document.getElementById('json').innerHTML = item["lastName"]      //the id in the html
+        let firstName = item ["firstName"];
+        //assigning the array value to a variable called first name
+        let lastName = item ["lastName"];
+        //make a list in the loop
+         example += `<li>${firstName}</li>`;
+         example += `<li>${lastName}</li>`;
+    });
+    example += "</ul>";
+    document.getElementById('json').innerHTML = example      //the id in the html
 
+    //how to write to the screen not the console
+
+    let myObj = {"firstName":"firstName","lastName":"lastName", "emailAddress":"emailAddress"};
+
+    let keysArray = Object.keys(myObj); //pull out the keys as a separate array
+
+    for(let i=0;i<keysArray.length;i++){
+      console.log(i); //will give the index, from 0 to the length of the keys array
+
+    let currentKey = keysArray[i]; //pull out the current key - "first", "second", etc - into a variable
+    
+    //now this line doesn't look nearly as scary as it did before!
+    console.log(myObj[currentKey]); //will give each value in turn, using the current key
+}
+    //format the text by consolelogging the array to the screen
+      //when you parse and object you take a string and make it an object
+      
+      console.log (writeText)                         //let assign variable
+      //document.getElementById('json').innerHTML = xhr.responseText      //the id in the html
+    } else if (xhr.status === 404) console.log('Error. File not found.')
+  }
+  xhr.send()
+}
 
 ajaxTxtBtn.addEventListener('click', loadtxt)
+ajaxJsonBtn.addEventListener('click', loadjson)
